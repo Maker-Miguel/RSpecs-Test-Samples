15a22fa4c79724830449fafd559e3135865a8cef
mmejia904 <mmejia904@gmail.com>
2019-10-31 11:02:06 -0500

    firts commit

---
+++ one_liner_syntax_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe 'shorthand syntax' do
+  subject{ 5 }
+
+  context 'with classic syntax' do
+    it 'should equal to 5' do
+      expect(subject).to eq(5)
+    end
+  end
+
+  context 'with classic syntax' do
+    it { is_expected.to eq(5) }
+  end
+
+end
